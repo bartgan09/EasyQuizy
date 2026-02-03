8aa9ebc9c681a0eb41fdaee7445b8b33905502a6
bartgan09 <bartgan09@icloud.com>
2026-02-03 10:45:04 +0100

    podstawowy szablon strony

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="icon" href="assets/favicon.png" type="image/png">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
+    </style>
+    <title>EasyQuizy</title>
+</head>
+<body>
+<header></header>
+<main></main>
+<section class="sidebar"></section>
+</body>
+</html>
